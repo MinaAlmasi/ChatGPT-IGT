259c6efdea1a1cb55d678e487d6f53dd52ba6aae
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-22 16:09:52 +0100

    add fn to handle empty decks

--- src/gpt_simulate.py
+++ src/gpt_simulate.py
@@ -88,6 +88,7 @@ def select_deck(client, task_desc, model_endpoint = 'gpt-3.5-turbo-1106', update
 
     return card_selection, messages
 
+
 def get_payoff(card_selection, decks, selected):
     # check card selection
     if card_selection not in ['A', 'B', 'C', 'D']:
@@ -108,8 +109,19 @@ def get_payoff(card_selection, decks, selected):
 
     return selected, win, loss
 
+def check_for_empty_deck(selected):
+    '''
+    Checks if any of the four decks have been selected 60 times
+    '''
+    # check if any of them are 60, if so return that deck
+    for deck in selected:
+        if selected[deck] >= 60:
+            return deck
+    
+    # if none of them are 60, return None
+    return None
 
-def update_messages(messages, card_selection, win, loss, total_earnings):
+def update_messages(messages, card_selection, win, loss, total_earnings, empty_deck):
     # update messages with card selection
     messages.append({"role": "assistant", "content": card_selection})
 
@@ -131,8 +143,21 @@ def update_messages(messages, card_selection, win, loss, total_earnings):
     # append win/loss message
     messages.append({"role": "user", "content": payoff_message})
 
-    # initialize next round
-    messages.append({"role": "user", "content": "A, B, C or D?"})
+    # initialize next round message based on empty deck also
+    if empty_deck is None:
+        messages.append({"role": "user", "content": "A, B, C or D?"})
+    
+    elif empty_deck == "A":
+        messages.append({"role": "user", "content": "Deck A is empty. B, C or D?"})
+    
+    elif empty_deck == "B":
+        messages.append({"role": "user", "content": "Deck B is empty. A, C or D?"})
+    
+    elif empty_deck == "C":
+        messages.append({"role": "user", "content": "Deck C is empty. A, B or D?"})
+    
+    elif empty_deck == "D":
+        messages.append({"role": "user", "content": "Deck D is empty. A, B or C?"})
 
     return messages
 
@@ -149,14 +174,15 @@ def save_data(data:dict, updated_messages, data_path):
 
     # set first column as 'trial'
     df.index.name = "trial"
-
+    df.reset_index(inplace=True)
+    
     # create filename with a date and time (by getting time, formatting it and then adding it to the filename)
     now = datetime.datetime.now()
     formatted_time = now.strftime("%Y%m%d_%H%M%S")
     filename = f"gpt_{formatted_time}"
 
     # save df into data 
-    df.to_csv(data_path/ f"{filename}.csv")
+    df.to_csv(data_path/ f"{filename}.csv", index=False)
 
     # save messages (logfile-ish)
     with open(log_path / f"{filename}_messages.txt", 'w') as f:
@@ -183,22 +209,29 @@ def main():
 
     # start with no message history 
     updated_messages = None
+    empty_deck = None
 
     while trials_played < trials_to_play:
+        ## TEMP CHUNK START ##
         # get chatgpt to select a card 
-        
         messages = [
             {"role": "system", "content": task_desc},
             {"role": "user", "content": task_desc + "\n" + "A, B, C or D?"}
         ]
 
-        # get card selection with numpy
-        card_selection = np.random.choice(['A', 'B', 'C', 'D'])
-        
+        # get card selection with numpy, weight A more 
+        if empty_deck == None:
+            card_selection = np.random.choice(['A', 'B', 'C', 'D'], p=[0.8, 0.05, 0.05, 0.1])
+        else:
+            card_selection = "B"
+        ## TEMP CHUNK END ##
         #card_selection, messages = select_deck(client, task_desc, updated_messages=updated_messages)
 
         # get payoff
         selected, win, loss = get_payoff(card_selection, decks, selected)
+
+        # check for empty decks
+        empty_deck = check_for_empty_deck(selected)
 
         # update total earnings based on wins and losses
         total_earnings = total_earnings + win - loss
@@ -210,15 +243,13 @@ def main():
         data[trials_played] = {"deck": card_selection, "gain": win, "loss": loss}
 
         # update messages
-        updated_messages = update_messages(messages, card_selection, win, loss, total_earnings)
+        updated_messages = update_messages(messages, card_selection, win, loss, total_earnings, empty_deck)
 
         # save data
         data_path = path / "data" / "gpt_data"
         save_data(data, updated_messages, data_path=data_path)
 
         print(trials_played, card_selection, win, loss)
-
-    #select_deck(client, task_desc)
 
 if __name__ == '__main__':
     main()
