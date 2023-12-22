5ddf997217f7d33661fbb3c46169af1f62162e35
MinaAlmasi <mina.almasi00@gmail.com>
2023-12-22 11:04:46 +0100

    update load data functions

--- src/load_data.py
+++ src/load_data.py
@@ -30,20 +30,16 @@ def load_ahn(data_path, filename:str="healthy_control"):
     '''
     Load Ahn data
     '''
+    # check if filename is valid
+    valid_filenames = ["healthy_control", "amphetamine", "heroin"]
+
+    if filename not in valid_filenames:
+        raise ValueError(f"filename must be one of {valid_filenames}")
+
     # load data
     df = pd.read_csv(data_path / f"IGTdata_{filename}.txt", sep="\t")
-    
-    # filter to only include a single deck
-    df = df[df['deck'] == 4]
 
-    # group by subject and sum
-    df = df.groupby(['subjID']).count()
-
-    # sort by highest deck 
-    df = df.sort_values(by=['deck'], ascending=False)
-    
-    print(df)
-    print(len(df))
+    return df 
 
 
 def main(): 
@@ -54,13 +50,12 @@ def main():
     data_root = path / "data" 
 
     # load data
-    df_stein = load_steingroever(data_root /  "IGTdataSteingroever2014")
+    #df_stein = load_steingroever(data_root /  "IGTdataSteingroever2014")
 
     # load ahn data
     df_ahn = load_ahn(data_path = data_root / "Ahn2014", filename="healthy_control")
 
     print(df_ahn)
-    print(df_stein)
 
 
 if __name__ == "__main__":
